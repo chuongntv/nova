c0233089060f5d573ab4e09633b90e33b072ee0e
chuongntv <chuongntv.1409@gmail.com>
2017-05-16 20:02:33 +0700

    Fix bug

--- nova/signature_utils.py
+++ nova/signature_utils.py
@@ -1,20 +1,3 @@
-# Copyright (c) The Johns Hopkins University/Applied Physics Laboratory
-# All Rights Reserved.
-#
-#    Licensed under the Apache License, Version 2.0 (the "License"); you may
-#    not use this file except in compliance with the License. You may obtain
-#    a copy of the License at
-#
-#         http://www.apache.org/licenses/LICENSE-2.0
-#
-#    Unless required by applicable law or agreed to in writing, software
-#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-#    License for the specific language governing permissions and limitations
-#    under the License.
-
-"""Support signature verification."""
-
 import binascii
 
 from castellan.common.exception import KeyManagerError
@@ -44,14 +27,11 @@ HASH_METHODS = {
     'SHA-512': hashes.SHA512(),
 }
 
-# Currently supported signature key types
 # RSA Options
 RSA_PSS = 'RSA-PSS'
 # DSA Options
 DSA = 'DSA'
 
-# ECC curves -- note that only those with key sizes >=384 are included
-# Note also that some of these may not be supported by the cryptography backend
 ECC_CURVES = (
     ec.SECT571K1(),
     ec.SECT409K1(),
@@ -61,14 +41,13 @@ ECC_CURVES = (
     ec.SECP384R1(),
 )
 
-# These are the currently supported certificate formats
 X_509 = 'X.509'
 
 CERTIFICATE_FORMATS = {
     X_509,
 }
 
-# These are the currently supported MGF formats, used for RSA-PSS signatures
+# MGF formats used for RSA-PSS signatures
 MASK_GEN_ALGORITHMS = {
     'MGF1': padding.MGF1,
 }
